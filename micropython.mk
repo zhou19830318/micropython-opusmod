2c1c35190a99015bb9b5e0646c5eff15ea2f7373
Conor <133641931+zhou19830318@users.noreply.github.com>
2025-06-25 11:32:19 +0800

    Update micropython.mk

--- micropython.mk
+++ micropython.mk
@@ -1,1 +1,18 @@
+MOD_DIR := $(USERMOD_DIR)
+SRC_USERMOD += $(MOD_DIR)/opusmod.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/opus.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/opus_decoder.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/opus_encoder.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/opus_multistream.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/opus_multistream_decoder.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/opus_multistream_encoder.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/repacketizer.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/analysis.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/mlp.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/mlp_data.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/silk/*.c
+SRC_USERMOD += $(MOD_DIR)/libopus/src/celt/*.c
 
+CFLAGS_USERMOD += -I$(MOD_DIR)/libopus/include
+CFLAGS_USERMOD += -DOPUS_BUILD
+CFLAGS_USERMOD += -Drestrict=
